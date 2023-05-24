4096d69c0447536619a7160e693519c2158c1478
Koz <kenneth.averna@gmail.com>
2023-05-24 13:54:14 -0700

    got the code for the lines but it was really bugging and made everything super slow, added a toggle for the groups and network pages

--- pages/section/main-page.tsx
+++ pages/section/main-page.tsx
@@ -4,6 +4,7 @@ import jsonGraph from "../../data/data.json";
 import AccountButton from "../../components/AccountButton";
 import UserSideMenu from "@/components/UserSideMenu";
 import { NostrConnectionContext } from "@/context/use-nostr-connection";
+import { line } from "d3-shape";
 import { nodeModuleNameResolver } from "typescript";
 
 interface Props {
@@ -16,6 +17,15 @@ export default function Chart({ graphData }: Props) {
    const width = 1400;
    const padding = 15;
    const data = jsonGraph.nodes;
+   const linesRef = useRef<any>();
+
+   // modify line data to match the format required by d3
+   const lineData = jsonGraph.edges.map(edge => ({
+      source: data.find(node => node.key === edge.source),
+      target: data.find(node => node.key === edge.target),
+      attributes: edge.attributes
+   }));
+
    // let data: any = [];
    // if (graphData) {
    //    try {
@@ -32,7 +42,23 @@ export default function Chart({ graphData }: Props) {
    function handleZoom(e: any) {
       d3.select('svg g')
          .attr('transform', e.transform);
+
+      //    linesRef.current.attr('d', (d: { source: { attributes: { x: any; y: any; }; }; target: { attributes: { x: any; y: any; }; }; }) => {
+      //    if (d.source && d.target) {
+      //       const sourceX = e.transform.applyX(d.source.attributes.x);
+      //       const sourceY = e.transform.applyY(d.source.attributes.y);
+      //       const targetX = e.transform.applyX(d.target.attributes.x);
+      //       const targetY = e.transform.applyY(d.target.attributes.y);
+      //       return lineGenerator([[sourceX, sourceY], [targetX, targetY]]);
+      //    } else {
+      //       return '';
+      //    }
+      // });
    }
+
+   const lineGenerator = line<[number, number]>()
+            .x(d => d[0])
+            .y(d => d[1]);
 
    useEffect(() => {
       if (result?.connection?.pubkey !== null) {
@@ -51,24 +77,33 @@ export default function Chart({ graphData }: Props) {
          }
 
          const simulation = d3.forceSimulation()
-               .force('link', d3.forceLink().id((d: any) => d.key))
-               .force('charge', d3.forceManyBody())
-               .force('center', d3.forceCenter(width/2, height/2));
+            .force('link', d3.forceLink().id((d: any) => d.key))
+            .force('charge', d3.forceManyBody())
+            .force('center', d3.forceCenter(width / 2, height / 2));
 
-         // simulation.nodes(jsonGraph.nodes);
-         // simulation.force('link').links(jsonGraph.edges);
-         
+         simulation.nodes(jsonGraph.nodes as d3.SimulationNodeDatum[]);
+         //simulation.force('link').links(jsonGraph.edges);
+
          const points = svg.select('g')
             .selectAll('circle')
             .data(data)
             .enter()
             .append("g");
-         
+
          points.append("circle")
             .attr('cx', function (d, i: number) { return data[i].attributes.x; })
             .attr('cy', function (d, i: number) { return data[i].attributes.y; })
             .attr('r', 4)
             .attr("fill", (d: any, i: number) => d3.interpolateRainbow(colorValue(data[i].attributes.x - data[i].attributes.y)));
+
+
+         // linesRef.current = svg.append('g')
+         //    .selectAll('path')
+         //    .data(lineData)
+         //    .enter()
+         //    .append('path')
+         //    .attr('stroke', 'white')
+         //    .attr('stroke-width', d => d.attributes.size);
 
          // Add links
          // const links = svg.selectAll('line')
@@ -77,7 +112,7 @@ export default function Chart({ graphData }: Props) {
 
          points
             .on("click", function (event, d) {
-               var index = data.findIndex(function(obj) {
+               var index = data.findIndex(function (obj) {
                   return obj.key == d.key
                })
                event.stopPropagation();
@@ -86,7 +121,7 @@ export default function Chart({ graphData }: Props) {
                   d3.zoomIdentity.translate(width / 2, height / 2).scale(40).translate(-data[index].attributes.x, -data[index].attributes.y)
                )
             });
-         
+
          points
             .on("mouseover", (event, d) => {
                console.log(d.key);
@@ -97,52 +132,52 @@ export default function Chart({ graphData }: Props) {
                      `<div class="dashboard-tooltip-label"></div>
                         <b>${d.attributes.label}</b>
                      `
-                  ); 
+                  );
             })
             .on("mousemove", (event) => {
                d3.select("#dashboard-tooltip")
-               .style("left", event.pageX + padding + "px")
-               .style("top", event.pageY + padding + "px");
+                  .style("left", event.pageX + padding + "px")
+                  .style("top", event.pageY + padding + "px");
             })
             .on("mouseleave", () => {
                d3.select("#dashboard-tooltip")
-               .style("opacity", 0)
-               .style("display", "none");
+                  .style("opacity", 0)
+                  .style("display", "none");
             });
 
-            // simulation.on('tick', () => {
+         // simulation.on('tick', () => {
 
-            //    links
-            //       .attr('x1', d => {
-            //          var val = 0;
-            //          const temp = data.find(node => d.source == node.key);
-            //          if (temp) val = temp.attributes.x;
-            //          return val;
-            //       })
-            //       .attr('y1', d => {
-            //          var val = 0;
-            //          const temp = data.find(node => d.source == node.key);
-            //          if (temp) val = temp.attributes.y;
-            //          return val;
-            //       })
-            //       .attr('x2', d => {
-            //          var val = 0;
-            //          const temp = data.find(node => d.target == node.key);
-            //          if (temp) val = temp.attributes.x;
-            //          return val;
-            //       })
-            //       .attr('y2', d => {
-            //          var val = 0;
-            //          const temp = data.find(node => d.target == node.key);
-            //          if (temp) val = temp.attributes.y;
-            //          return val;
-            //       });
-           
-            //    points
-            //       .attr('cx', d => d.attributes.x)
-            //       .attr('cy', d => d.attributes.y);
-            // });
-         
+         //    links
+         //       .attr('x1', d => {
+         //          var val = 0;
+         //          const temp = data.find(node => d.source == node.key);
+         //          if (temp) val = temp.attributes.x;
+         //          return val;
+         //       })
+         //       .attr('y1', d => {
+         //          var val = 0;
+         //          const temp = data.find(node => d.source == node.key);
+         //          if (temp) val = temp.attributes.y;
+         //          return val;
+         //       })
+         //       .attr('x2', d => {
+         //          var val = 0;
+         //          const temp = data.find(node => d.target == node.key);
+         //          if (temp) val = temp.attributes.x;
+         //          return val;
+         //       })
+         //       .attr('y2', d => {
+         //          var val = 0;
+         //          const temp = data.find(node => d.target == node.key);
+         //          if (temp) val = temp.attributes.y;
+         //          return val;
+         //       });
+
+         //    points
+         //       .attr('cx', d => d.attributes.x)
+         //       .attr('cy', d => d.attributes.y);
+         // });
+
       }
    }, [data]);
 
